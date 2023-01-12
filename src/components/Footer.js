3dc305bb2c6eefb0673fb6bc9b378f05ec91197a
Seoyeon Park <68735700+syeonnn@users.noreply.github.com>
2023-01-12 15:17:16 +0900

    Add files via upload

---
+++ src/components/Footer.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export default function Footer() {
+    return (
+    <footer className="bg-darkgray py-5">
+        <div className="container px-4 px-lg-5"><div className="small text-center text-muted">Copyright © 2023 - INFormation </div></div>
+    </footer>
+)}
